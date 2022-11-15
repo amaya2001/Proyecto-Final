4d88e8fb9624f16d1d22315a5f6e35f3f28c533b
Juan <sebas.amayar@gamil.com>
2022-11-15 09:57:56 -0500

    Clave incriptada y aceptacion de caracteres alfanumericos

--- ProyectoSemestral/Controllers/AccesoController.cs
+++ ProyectoSemestral/Controllers/AccesoController.cs
@@ -2,40 +2,77 @@
 using ProyectoSemestral.Models;
 using System.Data.SqlClient;
 using System.Data;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace ProyectoSemestral.Controllers
 {
     public class AccesoController : Controller
     {
+            
         public IActionResult Login()
         {
             return View();
         }
         static string cadena = "Data Source = DESKTOP-AT13GP0\\MSSQLSERVER_1; initial Catalog = DBPROYECTO; Integrated Security = true";
+        public static string ConvertirClave(string txt)
+        {
+            StringBuilder Sb = new StringBuilder();
+            using (SHA256 hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(txt));
+
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+            return Sb.ToString();
+        }
 
         [HttpPost]
         public IActionResult Login(Administrador oAdministrador)
         {
-            using (SqlConnection cn = new SqlConnection(cadena))
+            oAdministrador.Clave = ConvertirClave(oAdministrador.Clave);
+            bool rpta;
+            try
             {
-                SqlCommand cmd = new SqlCommand("sp_ValidarUsuario", cn);
-                cmd.Parameters.AddWithValue("Correo", oAdministrador.Correo);
-                cmd.Parameters.AddWithValue("Clave", oAdministrador.Clave);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cn.Open();
-                oAdministrador.IdAdministrador = Convert.ToInt32(cmd.ExecuteScalar().ToString());
+                
+                using (SqlConnection cn = new SqlConnection(cadena))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_ValidarUsuario", cn);
+                    if (oAdministrador.Correo.All(char.IsLetterOrDigit))
+                    {
+                        cmd.Parameters.AddWithValue("Correo", oAdministrador.Correo);
+                    }
+                    else
+                    {
+                        ViewData["Mensaje"] = "Solo caracteres alfanumericos";
+                        return View();
+                    }
+                    cmd.Parameters.AddWithValue("Clave", oAdministrador.Clave);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cn.Open();
+                    oAdministrador.IdAdministrador = Convert.ToInt32(cmd.ExecuteScalar().ToString());
 
-            }
-            if (oAdministrador.IdAdministrador != 0)
-            {
+                }
+                rpta = false;
+                if (oAdministrador.IdAdministrador != 0)
+                {
 
-                return RedirectToAction("Menu", "Mantenedor");
+                    return RedirectToAction("Menu", "Mantenedor");
+                }
+                else
+                {
+                    ViewData["Mensaje"] = "Usuario no encontrado";
+                    return View();
+                }
             }
-            else
+            catch (Exception e)
             {
-                ViewData["Mensaje"] = "Usuario no encontrado";
-                return View();
+                string error = e.Message;
+                rpta = false;
             }
+            return View();
         }
     }
 }
--- ProyectoSemestral/Models/Administrador.cs
+++ ProyectoSemestral/Models/Administrador.cs
@@ -1,9 +1,13 @@
-﻿namespace ProyectoSemestral.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoSemestral.Models
 {
     public class Administrador
     { 
         public int IdAdministrador { get; set; }
+        //[Required(ErrorMessage = "El campo Nombre es obligatorio")]
         public string? Correo { get; set; }
+        //[Required(ErrorMessage = "El campo Clave es obligatorio")]
         public string? Clave { get; set; }
     }
 }
